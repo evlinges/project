9f557a0d790d22ae0d8b81fb7e5b73a5c5747649
evlinges <evelina111tkachuk@gmail.com>
2025-04-24 01:27:52 +0300

    Виведений слайдер + адаптив

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      infinite: true,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: true,
+      prevArrow: '<button class="slick-prev"></button>',
+      nextArrow: '<button class="slick-next"></button>',
+    });
+  });
+  
